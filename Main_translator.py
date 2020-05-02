6a5989726b3c122701ebe506ebfbe69bb9a07efa
minusmagis <33642118+minusmagis@users.noreply.github.com>
2020-05-02 09:11:36 +0200

    Now it works for japanese subtitles as well and is less susceptible to give out of index errors

--- Main_translator.py
+++ Main_translator.py
@@ -11,14 +11,14 @@ def split(word):
 translator = Translator()
 # print(translator.translate('Hola',dest= 'ca',src='es'))
 
-Source_language = 'es'
-Destination_language = 'ca'
+Source_language = 'en'
+Destination_language = 'ja'
 
-# Subtitle_file = filedialog.askopenfilename(title='Select the input subtitle file', initialdir=r'C:\Users\minus\Documents\Fedellando\Video 5 Sand Filter\Subtitles', filetypes=(('sbv', '*.sbv'), ('All files', '*.*')))  # Prompt the user to open a file that contains the subtitle file to be translated.
+Subtitle_file = filedialog.askopenfilename(title='Select the input subtitle file', initialdir=r'C:\Users\minus\Documents\Fedellando\Video 5 Sand Filter\Subtitles', filetypes=(('sbv', '*.sbv'), ('All files', '*.*')))  # Prompt the user to open a file that contains the subtitle file to be translated.
 Save_file_directory = filedialog.askdirectory(title='Select the directory where you want to save the resulting subtitle file', initialdir=r'C:\Users\minus\Documents\Fedellando\Video 5 Sand Filter\Subtitles', mustexist=True)  # Ask for the directory where the resulting subtitle file will be saved
 
 
-Subtitle_file = r'C:\Users\minus\Documents\Fedellando\Video 5 Sand Filter\Subtitles\Full spanish captions.sbv'
+# Subtitle_file = r'C:\Users\minus\Documents\Fedellando\Video 5 Sand Filter\Subtitles\Full spanish captions.sbv'
 Subtitle_text = open(Subtitle_file, "r",encoding='utf-8')
 Subtitle_text_array = Subtitle_text.read().split('\n')
 # print(Subtitle_text.read().split('\n'))
@@ -82,51 +82,90 @@ for string in Sentence_string_list:
         translated_string_list.append(translated_sentence.text)
         # translated_string_list.append(translated_sentence)
 
-# print(*translated_string_list,sep='\n')
+print(*translated_string_list,sep='\n')
 
 translated_string_list_formatted = list()
 
+if Destination_language != 'ja':
+    for index,translated_sentence_full in enumerate(translated_string_list):
+        if len(Meta_sentence_string_list[index])>=2:
+            translated_sentence_full_temporal = split(translated_sentence_full)
+            # print('------------Loop start-------------')
+            # print(translated_sentence_full)
+            # print(Meta_sentence_string_list[index])
+            # print(len(Meta_sentence_string_list[index]))
+            White_space_ammount = 0
+            White_space_indexes = []
+            Cutting_whitespaces = list()
+            for sub_index,character in enumerate(translated_sentence_full):
+                if character == ' ':
+                    White_space_ammount += 1
+                    White_space_indexes.append(sub_index)
+            for index_1,_ in enumerate(Meta_sentence_string_list[index]):
+                commafound = False
+                if index_1 == 0:
+                    continue
+                else:
+                    white_space_to_cut = White_space_indexes[math.floor(index_1*White_space_ammount/len(Meta_sentence_string_list[index]))]
+                    Cutting_whitespaces.append(white_space_to_cut)
+                    for i in range(40):
+                        if translated_sentence_full_temporal[min(max(0,white_space_to_cut - 20 +i),len(translated_sentence_full)-1)] == ',':
+                            translated_sentence_full_temporal[min(max(0,white_space_to_cut - 20 +i),len(translated_sentence_full)-1)] = ',ß'
+                            commafound = True
+                            break
+                    if not commafound:
+                        translated_sentence_full_temporal[white_space_to_cut] = 'ß'
 
-for index,translated_sentence_full in enumerate(translated_string_list):
-    if len(Meta_sentence_string_list[index])>=2:
-        translated_sentence_full_temporal = split(translated_sentence_full)
-        # print('------------Loop start-------------')
-        # print(translated_sentence_full)
-        # print(Meta_sentence_string_list[index])
-        # print(len(Meta_sentence_string_list[index]))
-        White_space_ammount = 0
-        White_space_indexes = []
-        Cutting_whitespaces = list()
-        for sub_index,character in enumerate(translated_sentence_full):
-            if character == ' ':
-                White_space_ammount += 1
-                White_space_indexes.append(sub_index)
-        for index_1,_ in enumerate(Meta_sentence_string_list[index]):
-            commafound = False
-            if index_1 == 0:
-                continue
-            else:
-                white_space_to_cut = White_space_indexes[math.floor(index_1*White_space_ammount/len(Meta_sentence_string_list[index]))]
-                Cutting_whitespaces.append(white_space_to_cut)
-                for i in range(40):
-                    if translated_sentence_full_temporal[white_space_to_cut-20+i] == ',':
-                        translated_sentence_full_temporal[white_space_to_cut - 20 + i] = ',ß'
-                        commafound = True
-                        break
-                if not commafound:
-                    translated_sentence_full_temporal[white_space_to_cut] = 'ß'
+            # for index_1, _ in enumerate(Meta_sentence_string_list[index]):
+            #
+            translated_sentence_full_temporal = ''.join(translated_sentence_full_temporal)
+            translated_sentence_full_temporal = translated_sentence_full_temporal.split('ß')
 
-        # for index_1, _ in enumerate(Meta_sentence_string_list[index]):
-        #
-        translated_sentence_full_temporal = ''.join(translated_sentence_full_temporal)
-        translated_sentence_full_temporal = translated_sentence_full_temporal.split('ß')
+            # print('White spaces = '+str(White_space_ammount)+','+str(Cutting_whitespaces)+','+str(White_space_indexes))
+            # print(translated_sentence_full_temporal)
 
-        # print('White spaces = '+str(White_space_ammount)+','+str(Cutting_whitespaces)+','+str(White_space_indexes))
-        # print(translated_sentence_full_temporal)
+            translated_string_list_formatted.append(translated_sentence_full_temporal)
+        else:
+            translated_string_list_formatted.append(translated_sentence_full)
 
-        translated_string_list_formatted.append(translated_sentence_full_temporal)
-    else:
-        translated_string_list_formatted.append(translated_sentence_full)
+else:
+    for index, translated_sentence_full in enumerate(translated_string_list):
+        if len(Meta_sentence_string_list[index]) >= 2:
+            translated_sentence_full_temporal = split(translated_sentence_full)
+            # print('------------Loop start-------------')
+            # print(translated_sentence_full)
+            # print(Meta_sentence_string_list[index])
+            # print(len(Meta_sentence_string_list[index]))
+            for index_1, _ in enumerate(Meta_sentence_string_list[index]):
+                commafound = False
+                if index_1 == 0:
+                    continue
+                else:
+                    place_to_cut = math.floor(index_1 * len(translated_sentence_full_temporal) / len(Meta_sentence_string_list[index]))
+                    for i in range(40):
+                        if translated_sentence_full_temporal[min(max(0,place_to_cut - 20 +i),len(translated_sentence_full_temporal)-1)] == ',':
+                            translated_sentence_full_temporal[min(max(0,place_to_cut - 20 +i),len(translated_sentence_full_temporal)-1)] = ',ß'
+                            commafound = True
+                            break
+                    if not commafound:
+                        translated_sentence_full_temporal[place_to_cut] = 'ß'
+
+            # for index_1, _ in enumerate(Meta_sentence_string_list[index]):
+            #
+            translated_sentence_full_temporal = ''.join(translated_sentence_full_temporal)
+            translated_sentence_full_temporal = translated_sentence_full_temporal.split('ß')
+
+            # print('White spaces = '+str(White_space_ammount)+','+str(Cutting_whitespaces)+','+str(White_space_indexes))
+            # print(translated_sentence_full_temporal)
+
+            translated_string_list_formatted.append(translated_sentence_full_temporal)
+        else:
+            translated_string_list_formatted.append(translated_sentence_full)
+
+
+
+
+
 
 # print(*translated_string_list_formatted,sep='\n')
 
